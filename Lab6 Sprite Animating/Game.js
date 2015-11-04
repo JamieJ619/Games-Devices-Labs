407829ca0b5ba5ab673494f20e78c2dc8832724f
Jamie Jackson <619jamiej619@gmail.com>
2015-11-04 10:59:11 +0000

    Sprite Animating for lab 6

--- Lab6 Sprite Animating/Game.js
+++ Lab6 Sprite Animating/Game.js
@@ -1,15 +1,27 @@
 var canvas;
 var ctx; 
 
-touchable = 'createTouch' in document;
-var touches;
+var xPos;
+var yPos;
+var imageWidth;
+var imageHeight;
 
-var img = new Image();   // Create new img element
-img.src = 'myImage.png'; // Set source path
+var counter;
+var currentFrame;
+
+var img= new Image();
+img.src="resources/image.png";
 
 function Game()
 {
 	initCanvas();
+	this.xPos = 10;
+	this.yPos = 10;
+	this.imageWidth = 277;
+	this.imageHeight = 70;
+
+	counter = 0;
+	currentFrame = 0;
 }
 
 function initCanvas()
@@ -25,5 +37,22 @@ function initCanvas()
 
 Game.prototype.gameLoop = function()
 {
-	
+	window.requestAnimationFrame(game.gameLoop);
+	counter++;
+	if (counter > 10) 
+	{
+		counter = 0;
+		this.currentFrame ++;
+		if (this.currentFrame >= 4) 
+		{
+			this.currentFrame = 0;
+		}
+	}
+
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+//8 things
+//currentframe * size of splice, 0, w , h, x, y, w,h 
+	ctx.drawImage(img, currentFrame*70, 0, 69, 70, 10, 10, 70, 70);
+	console.log(this.yPos, this.xPos)
+	//ctx.drawImage(img, (69 * currentFrame),0,69, this.imageHeight,this.xPos, this.yPos, 100 ,80);
 }
