2243102204be69f85f70ca21e35f86d97e799890
Jamie Jackson <619jamiej619@gmail.com>
2016-01-22 10:29:23 +0000

    Lab 7 Websockets

--- Lab6 Sprite Animating/Game.js
+++ Lab6 Sprite Animating/Game.js
@@ -7,9 +7,13 @@ var imageWidth;
 var imageHeight;
 
 var counter;
+var counter2;
 var currentFrame;
+var counterFrame2;
 
 var img= new Image();
+var img2 = new Image();
+img2.src = "resources/image.png";
 img.src="resources/image.png";
 
 function Game()
@@ -21,7 +25,10 @@ function Game()
 	this.imageHeight = 70;
 
 	counter = 0;
+	counter2 = 0;
 	currentFrame = 0;
+	currentFrame2 = 0;
+ 	this.lastUpdate = Date.now();
 }
 
 function initCanvas()
@@ -35,11 +42,18 @@ function initCanvas()
 	canvas.height = window.innerHeight;
 }
 
+
+
+
 Game.prototype.gameLoop = function()
 {
 	window.requestAnimationFrame(game.gameLoop);
-	counter++;
-	if (counter > 10) 
+    var now = Date.now();
+    var dt = now - this.lastUpdate;
+    lastUpdate = now;
+	counter+= dt;
+	counter2+= dt;
+	if (counter > 20) 
 	{
 		counter = 0;
 		this.currentFrame ++;
@@ -48,11 +62,19 @@ Game.prototype.gameLoop = function()
 			this.currentFrame = 0;
 		}
 	}
+	if (counter2 > 50) 
+	{
+		counter2 = 0;
+		this.currentFrame2 ++;
+		if (this.currentFrame2 >= 4) 
+		{
+			this.currentFrame2 = 0;
+		}
+	}
+
 
 	ctx.clearRect(0,0,canvas.width,canvas.height);
-//8 things
-//currentframe * size of splice, 0, w , h, x, y, w,h 
 	ctx.drawImage(img, currentFrame*70, 0, 69, 70, 10, 10, 70, 70);
+	ctx.drawImage(img2, currentFrame2*70, 0, 69, 70, 10, 100, 70, 70);
 	console.log(this.yPos, this.xPos)
-	//ctx.drawImage(img, (69 * currentFrame),0,69, this.imageHeight,this.xPos, this.yPos, 100 ,80);
 }
